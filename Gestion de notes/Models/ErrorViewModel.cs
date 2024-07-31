a1fca4f083d4c5f92066f5d5ab94a0f4e93d9c11
ASUSX555 <ASUSX555@ASUS>
2024-07-31 22:27:01 +0300

    Ajoutez des fichiers projet.

---
+++ Gestion de notes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace gestion_de_notes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
